d87c9345a8af6a74af317c81d2ee9cf1cedb2c71
Max Froumentin <max@e97.me>
2023-06-03 22:12:19 +0100

    Level 100

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
-
-const videoData = [
-  {
-    "id": 523523,
-    "title": "Never Gonna Give You Up",
-    "url": "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    "rating": 23
-  },
-  {
-    "id": 523427,
-    "title": "The Coding Train",
-    "url": "https://www.youtube.com/embed/HerCR8bw_GE",
-    "rating": 230
-  },
-  {
-    "id": 82653,
-    "title": "Mac & Cheese | Basics with Babish",
-    "url": "https://www.youtube.com/embed/FUeyrEN14Rk",
-    "rating": 2111
-  },
-  {
-    "id": 858566,
-    "title": "Videos for Cats to Watch - 8 Hour Bird Bonanza",
-    "url": "https://www.youtube.com/embed/xbs7FT7dXYc",
-    "rating": 11
-  },
-  {
-    "id": 453538,
-    "title": "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
-    "url": "https://www.youtube.com/embed/4As0e4de-rI",
-    "rating": 3211
-  },
-  {
-    "id": 283634,
-    "title": "Learn Unity - Beginner's Game Development Course",
-    "url": "https://www.youtube.com/embed/gB1F9G0JXOo",
-    "rating": 211
-  },
-  {
-    "id": 562824,
-    "title": "Cracking Enigma in 2021 - Computerphile",
-    "url": "https://www.youtube.com/embed/RzWB5jL5RX0",
-    "rating": 111
-  },
-  {
-    "id": 442452,
-    "title": "Coding Adventure: Chess AI",
-    "url": "https://www.youtube.com/embed/U4ogK0MIzqk",
-    "rating": 671
-  },
-  {
-    "id": 536363,
-    "title": "Coding Adventure: Ant and Slime Simulations",
-    "url": "https://www.youtube.com/embed/X-iSQQgOd1A",
-    "rating": 76
-  },
-  {
-    "id": 323445,
-    "title": "Why the Tour de France is so brutal",
-    "url": "https://www.youtube.com/embed/ZacOS8NBK6U",
-    "rating": 73
-  }
-]
+import videoData from "./videos.json";
 
 
 const YouTubeEmbed = ({ embedUrl, title }) => {
@@ -80,51 +18,49 @@ const YouTubeEmbed = ({ embedUrl, title }) => {
 };
 
 
-const Video = ({ id, embedUrl, title, rating }) => {
-  const [visible, setVisible] = useState(true);
-  const [votes, setVotes] = useState(rating);
-
-  const increase = () => setVotes(votes => votes+1);
-  const decrease = () => setVotes(votes => votes-1);
-
-  const hide = function() {
-    setVisible(visible => !visible);
-  };
-
-  if (visible) {
-    return (
-      <div>
-        <h2>{title}</h2>
-        <h3>{votes} votes</h3>
-        <YouTubeEmbed embedUrl={embedUrl} title={title}/>
-        <br/>
-        <button onClick={hide}>Remove</button>
-        <button onClick={increase}>Up vote</button>
-        <button onClick={decrease}>Down vote</button>
-      </div>
-    );
-  } else {
-    return (<div></div>);
-  }
+const Video = ({ id, title, embedUrl, upVoteFn, downVoteFn, rating }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h3>{rating} votes</h3>
+      <YouTubeEmbed embedUrl={embedUrl} title={title}/>
+      <br/>
+      <button onClick={upVoteFn}>Up vote</button>
+      <button onClick={downVoteFn}>Down vote</button>
+    </div>
+  );
 };
 
 
-const AddVideo = function() {
-  return (
-    <div>
-    </div>
-  );
-}
-
 function App() {
-  const [videos, setVideos] = useState(videoData);
+  const [ videos, setVideos ] = useState(videoData);
+  const upvote = function(id) {
+    return function() {
+      setVideos(videos => videos.map(video => {
+        return {
+          id: video.id,
+          title: video.title,
+          url: video.url,
+          rating: video.id === id ? video.rating + 1 : video.rating
+        };
+      }));
+    };
+  };
+  const downvote = function(event) {
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-        {videos.map(vid => <Video id={vid.id} embedUrl={vid.url} title={vid.title} rating={vid.rating}/>)}
-      </header>
-      <AddVideo/>
+      <h1>Video Recommendation</h1>
+      {videoData.map(vid =>
+        <Video
+          key={vid.id}
+          id={vid.id}
+          embedUrl={vid.url}
+          upVoteFn={upvote(vid.id)}
+          downVoteFn={downvote(vid.id)}
+          rating={vid.rating}
+        />
+      )}
     </div>
   );
 }
